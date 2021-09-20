323376d1358250409328cc0978a1eb8831d38449
luisluft <luberluft@gmail.com>
2021-09-20 08:05:39 -0400

    implement buttons and auto slider features

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+
+    if (index < 0) setIndex(lastIndex);
+    if (index > lastIndex) setIndex(0);
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <section className="section">
       <div className="title">
@@ -15,11 +30,19 @@ function App() {
         </h2>
       </div>
       <div className="section-center">
-        {people.map((person, index) => {
+        {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
 
+          let sliderClass = "nextSlide";
+          if (personIndex === index) sliderClass = "activeSlide";
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          )
+            sliderClass = "prevSlide";
+
           return (
-            <article key={id}>
+            <article className={sliderClass} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -29,10 +52,10 @@ function App() {
           );
         })}
 
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
